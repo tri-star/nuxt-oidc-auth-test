527b92b53d815734e107ad45f1a0e8e0ecaff82d
tristar <tristar.2nd@gmail.com>
2024-10-27 23:48:36 +0900

    Add OIDC configuration, index page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-oidc-auth']
-})
+  modules: ['nuxt-oidc-auth'],
+  oidc: {
+    providers: {
+      auth0: {
+        redirectUri: "http://localhost:3000/auth/auth0/callback",
+        baseUrl: process.env.NUXT_OIDC_PROVIDERS_AUTH0_BASE_URL,
+        clientId: "",
+        clientSecret: "",
+        // skipAccessTokenParsing: true,
+        scope: ["openid", "profile", "email"],
+      },
+    },
+  },})
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const {user} = useOidcAuth();
+</script>
+
+<template>
+  <div>User: {{ user }}</div>
+</template>
+
+<style scoped>
+</style>
